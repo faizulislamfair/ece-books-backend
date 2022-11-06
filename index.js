d9f308e7e382d153ef880f0c7842e229323ab7cf
faizulislamfair <faizulislamfair@gmail.com>
2022-11-06 22:52:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Ece Books Server is Running!');
+})
+
+
+app.listen(port, () => {
+    console.log(`Ece Books Listening on Port: ${port}`);
+})
