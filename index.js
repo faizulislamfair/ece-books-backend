a6166ae902bb45161a37fb2bd1a454f0714aa9e6
faizulislamfair <faizulislamfair@gmail.com>
2022-11-07 11:16:18 +0600

    send user to server

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middlewares
 app.use(cors());
 app.use(express.json());
 
 
+
+const uri = "mongodb+srv://ece-books:nAQs2gPHg033QcmB@cluster0.pqumcav.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const bookCollection = client.db('eceBooks').collection('books');
+
+        // const user = {
+        //     name: 'someone',
+        //     comment: 'nice'
+        // }
+
+        // const result = await bookCollection.insertOne(user);
+        // console.log(result);
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err));
+
+
+
+
 app.get('/', (req, res) => {
-    res.send('Ece Books Server is Running!');
+    res.send('<h1 style="text-align: center">Ece Books Server is Running!</h1>');
 })
 
 
